eb67e671f1405f3c861e6d4ee6f522e232d3e871
sigurdk <sigurd.kolltveit@nhn.no>
2022-09-13 12:41:55 +0200

    Change Extension name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,13 +10,13 @@ class Extension : BurpExtension {
         val ui = api.userInterface()
         val http = api.http()
 
-        api.misc().setExtensionName("Sigurds Extension")
+        api.misc().setExtensionName("Sigurds JSON")
 
         val settings = Settings(true, true)
 
         http.registerHttpHandler(MyHTTPHandler(http, settings))
 
-        ui.registerSuiteTab("Sigurds Extension", SuiteTab(logging, settings))
+        ui.registerSuiteTab("Sigurds JSON", SuiteTab(logging, settings))
         ui.registerContextMenuItemsProvider(ContextMenu(logging, http))
     }
 }
